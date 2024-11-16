1aa6a41b76e2641de1eda38cf7eef666a1b9a319
NyxoraXv <87947067+NyxoraXv@users.noreply.github.com>
2024-11-16 13:57:19 +0700

    c

--- lung_cancer_prediction/lung_cancer_prediction/settings.py
+++ lung_cancer_prediction/lung_cancer_prediction/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,7 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  # URL for serving static files in development
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Additional static files directories
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- lung_cancer_prediction/predictor_webapp/urls.py
+++ lung_cancer_prediction/predictor_webapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.text_based_predict, name='txtpredict'),
     path('formpredict/', views.text_based_predict, name='txtpredict'),  # Your home page with the form
     path('predictinput/', views.input_predict, name='predict_request'),  # The endpoint to handle the form submission
     path('predictimage/', views.image_predict, name='predictimage'),  # Updated this name
